a2ef4b78ef2931db0e3530af0c53f16a9b3756db
Nathalie Moraga <129231844+NatieMor@users.noreply.github.com>
2023-07-21 19:49:57 -0400

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    #def after_sign_in_path_for(resource)
+     #   news_path
+    #end
+
+ #   def authorize_request(kind = nil)
+   #     unless kind.include?(current_user.role)
+  #      redirect_to news_index_path, notice: "You are not authorized to perform this action"
+  #      end
+ #   end
+#end
 end
--- app/controllers/publicacions_controller.rb
+++ app/controllers/publicacions_controller.rb
@@ -1,6 +1,20 @@
 class PublicacionsController < ApplicationController
   before_action :set_publicacion, only: %i[ show edit update destroy ]
 
+   #usuario normal
+  before_action only: [:index, :show] do
+    authorize_request(["normal","ejecutivo","admin"])
+  end 
+  #usuario ejecutivo
+  before_action only: [:new, :edit, :create, :update] do
+    authorize_request(["ejecutivo","admin"])
+  end
+
+  #usuario admin
+  before_action only: [:destroy] do
+    authorize_request(["admin"])
+  end
+
   # GET /publicacions or /publicacions.json
   def index
     @publicacions = Publicacion.all
